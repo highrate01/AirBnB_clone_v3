f5f6014a00fda574f594b01b42bf9a27b1015e02
Olufemi Adebisi <olufemiadebisi35@gmail.com>
2024-06-08 14:26:07 +0000

    updated

--- api/v1/app.py
+++ api/v1/app.py
@@ -9,7 +9,11 @@ from flask import Flask
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+app.teardown.appcontext
 
+def teardown():
+    """close engine"""
+    storage.close()
 
 if __name__ == "__main__":
     HOST = getenv("HBNB_API_HOST", '0.0.0.0')
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,6 +2,7 @@
 """defines api status"""
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
 
 
 @app_views.route('/status')
@@ -9,3 +10,23 @@ def api_status():
     """return route"""
     response = {"status": "OK"}
     return jsonify(response)
+
+@app_views.route('/stats')
+def get_stats():
+    """retrieves the number of each objects by type"""
+
+    stats = {
+            "amenities": storage.count('Amenity'),
+            "cities": storage.count('City'),
+            "places": storage.count('Place'),
+            "reviews": storage.count('Review'),
+            "states": storage.count('State'),
+            "users": storage.count('User')
+            }
+    return jsonify(stats)
+
+@app_views.errorhandler(404)
+def not_found(error):
+    """custom error handler"""
+    response = {"error": "Not found"}
+    return jsonfy(response), 404
